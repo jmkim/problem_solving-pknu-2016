d0c16071d5e8f0ddb07c43fec45e200174e6e5c8
Jongmin Kim <kdzlvaids@gmail.com>
2016-03-16 14:32:22 +0900

    Fix conditional expression

--- practice02/05.cpp
+++ practice02/05.cpp
@@ -1,15 +1,19 @@
+#include <stdio.h>
 #include <stdbool.h>
 
-struct interval
+bool is_two_intervals_overlap(const int a, const int b, const int c, const int d)
 {
-    int begin;
-    int end;
-};
-
-bool is_two_intervals_overlap(const struct interval *a, const struct interval *b)
-{
-    if( (a->begin < b->begin && a->end < b->begin) &&
-        (a->begin > b->begin && a->begin > b->end)) return false;
-
+    if(a < c && b < c || a > c && a > d) return false;
     return true;
+}
+
+int main(void)
+{
+    printf("%s\n", is_two_intervals_overlap(1, 3, 5, 6) ? "yes" : "no");
+    printf("%s\n", is_two_intervals_overlap(5, 6, 1, 3) ? "yes" : "no");
+    printf("%s\n", is_two_intervals_overlap(1, 3, 2, 4) ? "yes" : "no");
+    printf("%s\n", is_two_intervals_overlap(2, 4, 1, 3) ? "yes" : "no");
+    printf("%s\n", is_two_intervals_overlap(3, 4, 1, 5) ? "yes" : "no");
+    printf("%s\n", is_two_intervals_overlap(1, 5, 3, 4) ? "yes" : "no");
+    return 0;
 }
